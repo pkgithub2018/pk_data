10c43a942e8e1cfd1553330da9b14fff830d0d62
pkgithub2018 <phaykeo@yahoo.com>
2025-08-01 13:42:22 +0700

    Updated

--- php-bin/users_dataprocess.php
+++ php-bin/users_dataprocess.php
@@ -68,8 +68,241 @@ if( isset($_POST['provinceid'])){
         while ($row = pg_fetch_assoc($result)) {
             echo "<option value=\"{$row['id']}\" $selected>{$row['dname']}</option>";
         }
-    }
+    }   
+}
+
+//5.) Enable and disable country 
+if(isset($_POST['countryid']) && isset($_POST['countrystatus'])){
+  $cid = $_POST['countryid'];
     
-     
+    $status = ($_POST['countrystatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbcountries SET enabled = '" . $status . "' WHERE id = '$cid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      echo "<script>window.location.href = 'masterdata.php?part=countries';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating location data.');</script>";
+    }   
+}
+
+//6.) Enable and disable product
+
+if(isset($_POST['productid']) && isset($_POST['productchboxstatus'])){
+    $pid = $_POST['productid'];
+    
+    $status = ($_POST['productchboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbproduct SET enabled = '" . $status . "' WHERE id = '$pid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=product';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating product data.');</script>";
+    }   
+}
+
+//7.) Enable and disable product group
+if(isset($_POST['productgroupid']) && isset($_POST['productgroupchboxstatus'])){
+    $pgid = $_POST['productgroupid'];
+    
+    $status = ($_POST['productgroupchboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbproduct_group SET enabled = '" . $status . "' WHERE id = '$pgid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=productgroup';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating product group data.');</script>";
+    }   
+}
+
+//8.) Enable and disable product unit
+if(isset($_POST['productunitid']) && isset($_POST['productunitchboxstatus'])){
+    $punitid = $_POST['productunitid'];
+    
+    $status = ($_POST['productunitchboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbproduct_unit SET enabled = '" . $status . "' WHERE id = '$punitid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=productunit';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating product unit data.');</script>";
+    }   
+}
+
+//9.) Enable and disable conveyance
+if(isset($_POST['conveyanceid']) && isset($_POST['conveyancechboxstatus'])){
+    $conveyid = $_POST['conveyanceid'];
+    
+    $status = ($_POST['conveyancechboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbconveyance SET enabled = '" . $status . "' WHERE id = '$conveyid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=conveyance';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating conveyance data.');</script>";
+    }   
+}
+//10.) Enable and disable inspection method  
+
+if(isset($_POST['inspectionmethodid']) && isset($_POST['inspectionmethodchboxstatus'])){
+    $imethodid = $_POST['inspectionmethodid'];
+    
+    $status = ($_POST['inspectionmethodchboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbinspection_method SET enabled = '" . $status . "' WHERE id = '$imethodid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=inspectionmethod';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating inspection method data.');</script>";
+    }   
+}
+
+//11.) Enable and disable treatment method 
+if(isset($_POST['treatmentmethodid']) && isset($_POST['treatmentmethodchboxstatus'])){
+    $tmid = $_POST['treatmentmethodid'];
+    
+    $status = ($_POST['treatmentmethodchboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbtreatment_method SET enabled = '" . $status . "' WHERE id = '$tmid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=treatmentmethod';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating treatment method data.');</script>";
+    }   
+}
+
+//12.) Enable and disable entity type
+if(isset($_POST['entitytypeid']) && isset($_POST['entitytypechboxstatus'])){
+    $etid = $_POST['entitytypeid'];
+    
+    $status = ($_POST['entitytypechboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbentity_type SET enabled = '" . $status . "' WHERE id = '$etid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=entitytype';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating entity type data.');</script>";
+    }   
+}
+
+//13.) Enable and disable module
+if(isset($_POST['moduleid']) && isset($_POST['modulechboxstatus'])){
+    $mid = $_POST['moduleid'];
+    
+    $status = ($_POST['modulechboxstatus'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbmodules SET enabled = '" . $status . "' WHERE id = '$mid'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=modules';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating module data.');</script>";
+    }   
+}
+
+//14.) Enable permit-delete
+if(isset($_POST['permitid_delete']) && isset($_POST['permitchboxstatus_delete'])){
+    
+    $pid_delete = $_POST['permitid_delete'];
+       
+    $status = ($_POST['permitchboxstatus_delete'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbgrouppermits SET pdelete = '" . $status . "' WHERE id = '$pid_delete'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=permits';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating permit data.');</script>";
+    }   
+} 
+//15.) Enable permit-edit
+if(isset($_POST['permitid_edit']) && isset($_POST['permitchboxstatus_edit'])){
+    
+    $pid_edit = $_POST['permitid_edit'];
+       
+    $status = ($_POST['permitchboxstatus_edit'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbgrouppermits SET pupdate = '" . $status . "' WHERE id = '$pid_edit'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=permits';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating permit data.');</script>";
+    }   
+}
+//16.) Enable permit-add 
+if(isset($_POST['permitid_add']) && isset($_POST['permitchboxstatus_add'])){
+    
+    $pid_add = $_POST['permitid_add'];
+       
+    $status = ($_POST['permitchboxstatus_add'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbgrouppermits SET padd = '" . $status . "' WHERE id = '$pid_add'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=permits';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating permit data.');</script>";
+    }   
+}
+//17.) Enable permit-read
+if(isset($_POST['permitid_read']) && isset($_POST['permitchboxstatus_read'])){
+    
+    $pid_read = $_POST['permitid_read'];
+       
+    $status = ($_POST['permitchboxstatus_read'] === 'true') ? 'yes' : 'no';
+       
+    $sqlupdate = "UPDATE tbgrouppermits SET pread = '" . $status . "' WHERE id = '$pid_read'";
+    $result = pg_query($con, $sqlupdate) or die(pg_last_error($con));
+  
+     if ($result) {
+      
+      echo "<script>window.location.href = 'masterdata.php?part=permits';</script>";
+      //exit();
+    } else {
+      echo "<script>alert('Error updating permit data.');</script>";
+    }   
 }
 ?>
